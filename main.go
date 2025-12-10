75401f307c88c8ca69a3551d731c2641e44fd876
InjamHossan <injamhossanmamun@gmail.com>
2025-12-10 21:37:22 +0600

    variables class

--- main.go
+++ main.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
+const LoginToken string = "gajdksljklgsdfgklbderalighberEa"
+
 func main() {
-	fmt.Println("Hello Bangladesh");
+	var username string = "Injam Hossan"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.54654465546464564
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	//default values and some aliases
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type
+	var website = "learncodeonline.in"
+	fmt.Println(website)
+
+	//no var style
+	numberOfUser := 300000.0
+	fmt.Println(numberOfUser)
+
+	fmt.Println("Variable is of type: %T \n", LoginToken)
 }
