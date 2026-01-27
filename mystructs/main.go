db2086ac3037d58a9311d6d8b008bcb2be75ea51
Injam Hossan Mamun <injamhossanmamun@gmail.com>
2026-01-28 01:15:07 +0600

    struct

---
+++ mystructs/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Structs is golang")
+
+	// No inheritance in go lang ; No super or parent
+
+	injam := User{"Injam", "injam@gmail.com", true, 16}
+	fmt.Println(injam)
+	fmt.Printf("Injam Details are: %+v\n", injam)
+	fmt.Printf("Name is %v and email is %v.", injam.Name, injam.Email)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
